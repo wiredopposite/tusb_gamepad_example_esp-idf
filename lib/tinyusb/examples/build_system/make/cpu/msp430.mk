9bc305341bf2d44cd11e28d69f1238c3a8e9fc5a
wiredopposite <jackxhorizon@gmail.com>
2024-04-22 11:38:00 -0600

    submodules

--- lib/tinyusb/examples/build_system/make/cpu/msp430.mk
+++
@@ -1,8 +0,0 @@
-ifeq ($(TOOLCHAIN),gcc)
-  # nothing to add
-else ifeq ($(TOOLCHAIN),iar)
-  # nothing to add
-endif
-
-# For freeRTOS port source
-FREERTOS_PORTABLE_SRC ?= $(FREERTOS_PORTABLE_PATH)/GCC_MSP430F449
